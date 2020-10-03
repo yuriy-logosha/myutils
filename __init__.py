1f0494435ba8f46846f304b8c9600df578223a9d
yura <yuriy.logosha@gmail.com>
2020-10-03 21:58:37 +0300

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+__title__ = 'myutils'
+__author__ = 'Iurii Logosha'
+__license__ = 'Apache License 2.0'
+__copyright__ = 'Copyright 2020 Iurii Logosha'
+
+__all__ = ['myanalyzer', 'myfile', 'myjson', 'mygoogle', 'mylogging', 'myparser', 'myrequests']
--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="myutils",
-    version="1.0"
-)
