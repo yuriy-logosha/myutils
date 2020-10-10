181963cff6befd830b8e5d966343d9ae56582c45
yura <yuriy.logosha@gmail.com>
2020-10-10 19:07:30 +0300

    Add get_current_volume into mymacbook.py

--- mymacbook.py
+++ mymacbook.py
@@ -13,3 +13,15 @@ def unlock():
 
 def set_input_volume(val):
     myosascript.run('set volume input volume %s' % val)
+
+
+def get_current_volume(type='output'):
+    cmd = 'get volume settings'
+    settings = myosascript.run(cmd)
+    # assert len(settings) == 1, "Wrong output: %s" % cmd
+    # assert type(settings[0]) == str, "Wrong type of: %s" % settings[0]
+    # assert len(settings[0].split(', ')) == 4, "Wrong format of: %s" % settings[0].split(', ')
+    # assert len(settings[0].split(', ')[0].split(':')) == 2, "Wrong format of: %s" % settings[0].split(', ')[0]
+    # assert type(settings[0].split(', ')[0].split(':')[1]) == str, "Wrong type of: %s" % settings[0].split(', ')[0].split(':')[1]
+    return int(settings[0].split(', ')[0].split(':')[1]) if type == 'output' else int(
+        settings[0].split(', ')[1].split(':')[1]) if type == 'input' else 0
